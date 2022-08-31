dc0a4093e860e637649d90be2fb0a86439d23fe8
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-08-31 20:37:19 +0800

    add changelog output

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import { getLoader, getMappings, getQFAPI } from "./versions";
 import { error, success } from "./logger";
 import { readFile, writeFile } from "fs/promises";
 
+import { setOutput } from "@actions/core";
+
 interface VersionsTOML {
   versions: { [key: string]: string };
   libraries: {
@@ -13,52 +15,73 @@ interface VersionsTOML {
   plugins: { [key: string]: { id: string; version: string } };
 }
 
+const upgrade = async (
+  name: string,
+  lib: string,
+  original: VersionsTOML,
+  newVersion: string,
+): Promise<[string, string] | null> => {
+  let actuallyUpgraded = false;
+
+  let versionObjInLib = original.libraries[lib].version;
+
+  if (typeof versionObjInLib === "string") {
+    if (versionObjInLib !== newVersion) {
+      actuallyUpgraded = true;
+      versionObjInLib = newVersion;
+    }
+  } else {
+    if (
+      original.versions[versionObjInLib.ref] !==
+        newVersion
+    ) {
+      actuallyUpgraded = true;
+      original.versions[versionObjInLib.ref] = newVersion;
+    }
+  }
+
+  if (actuallyUpgraded) {
+    success(`Updated ${name} to ${newVersion}`);
+    return [name, newVersion];
+  }
+
+  success(`${name} is up to date!`);
+  return null;
+};
+
 (async () => {
   const textToml = await readFile("./gradle/libs.versions.toml", "utf8");
   const original = parse(textToml) as unknown as VersionsTOML;
 
   const mcVersion = original.versions.minecraft;
 
+  let updatedStuff: [string, string][] = [];
+
   for (const lib in original.libraries) {
-    const libObj = original.libraries[lib];
-
-    if (libObj.module === "org.quiltmc:quilt-mappings") {
+    if (original.libraries[lib].module === "org.quiltmc:quilt-mappings") {
       const mappings = await getMappings(mcVersion);
-
-      if (typeof libObj.version === "string") {
-        original.libraries[lib].version = mappings;
-      } else {
-        // @ts-expect-error typescript did a oopsie here
-        original.versions[original.libraries[lib].version.ref] = mappings;
-      }
-
-      success(`Updated Quilt Mappings to ${mappings}`);
-    } else if (libObj.module === "org.quiltmc:quilt-loader") {
+      const res = await upgrade("Quilt Mappings", lib, original, mappings);
+      if (res) updatedStuff.push(res);
+    } else if (original.libraries[lib].module === "org.quiltmc:quilt-loader") {
       const loader = await getLoader(mcVersion);
-
-      if (typeof libObj.version === "string") {
-        original.libraries[lib].version = loader;
-      } else {
-        // @ts-expect-error typescript did a oopsie here
-        original.versions[original.libraries[lib].version.ref] = loader;
-      }
-
-      success(`Updated Quilt Loader to ${loader}`);
+      const res = await upgrade("Quilt Loader", lib, original, loader);
+      if (res) updatedStuff.push(res);
     } else if (
-      libObj.module === "org.quiltmc.quilted-fabric-api:quilted-fabric-api"
+      original.libraries[lib].module ===
+        "org.quiltmc.quilted-fabric-api:quilted-fabric-api"
     ) {
       const qfapi = await getQFAPI(mcVersion);
-
-      if (typeof libObj.version === "string") {
-        original.libraries[lib].version = qfapi;
-      } else {
-        // @ts-expect-error typescript did a oopsie here
-        original.versions[original.libraries[lib].version.ref] = qfapi;
-      }
-
-      success(`Updated Quilted Fabric API to ${qfapi}`);
+      const res = await upgrade("Quilted Fabric API", lib, original, qfapi);
+      if (res) updatedStuff.push(res);
     }
   }
+
+  setOutput(
+    "changelog",
+    updatedStuff.map(([name, version]) =>
+      `- **${name}** was updated to \`${version}\``
+    ).join("\n"),
+  );
 
   await writeFile(
     "./gradle/libs.versions.toml",
